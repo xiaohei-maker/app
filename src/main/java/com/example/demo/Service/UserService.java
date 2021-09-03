49b4a6d572acc0a62bd64f085cbfddfb9438664a
小黑 <76620834+xiaohei-maker@users.noreply.github.com>
2021-09-03 11:23:04 +0800

    Update UserService.java

--- src/main/java/com/example/demo/Service/UserService.java
+++ src/main/java/com/example/demo/Service/UserService.java
@@ -39,15 +39,17 @@ public class UserService {
     }
 
     public String createUser(User user) {
-        UserExample userExample = new UserExample();
+       UserExample userExample = new UserExample();
         userExample.createCriteria()
                 .andAccountIdEqualTo(user.getAccountId());
         List<User> users = userMapper.selectByExample(userExample);
-
+        if(users.size() != 0){
+            return  null;
+        }
         user.setGmtCreate(System.currentTimeMillis());
         user.setGmtModified(user.getGmtCreate());
-        userMapper.insert(user);
-        return  null;
+        String a= String.valueOf(userMapper.insert(user));
+        return  a;
     }
 
 //    //平常的登录和修改
